a26c7d1ed8476be78b8b4c2fea0ddf036bf3fb18
Mahii-Botz <herokutest31@gmail.com>
2025-05-26 11:22:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-__path = process.cwd();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
-let code = require("./pair");
 require("events").EventEmitter.defaultMaxListeners = 500;
+
+// Serve static files (like HTML, CSS, JS) from root folder
+app.use(express.static(__dirname)); // ⬅️ This is important
+
+// API route
+const code = require("./pair");
 app.use("/code", code);
 
-app.use("/", async (req, res, next) => {
-  res.sendFile(__path + "/index.html");
-});
-
+// Body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => {
-  console.log(`⏩ Server running on http://localhost:` + PORT);
+  console.log(`⏩ Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
